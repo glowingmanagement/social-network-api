f47252c9de48a925448b070c31522f645085ab7e
Josh Holmes <josh@glowingmanagement.com>
2022-08-06 16:58:59 +0100

    delete friend

--- src/controllers/api/friends.js
+++ src/controllers/api/friends.js
@@ -2,13 +2,18 @@ const { User } = require("../../models");
 
 const createNewFriend = async (req, res) => {
   const { userId, friendId } = req.params;
-  console.log(req.params);
 
   try {
     if (userId && friendId) {
       await User.findOneAndUpdate(
         { _id: userId },
         { $addToSet: { friends: friendId } },
+        { new: true, runValidators: true }
+      );
+
+      await User.findOneAndUpdate(
+        { _id: friendId },
+        { $addToSet: { friends: userId } },
         { new: true, runValidators: true }
       );
       return res.json({ success: true });
@@ -19,8 +24,28 @@ const createNewFriend = async (req, res) => {
   }
 };
 
-const deleteFriend = (req, res) => {
-  const { id } = req.params;
+const deleteFriend = async (req, res) => {
+  const { userId, friendId } = req.params;
+
+  try {
+    if (userId && friendId) {
+      await User.findOneAndUpdate(
+        { _id: userId },
+        { $pull: { friends: friendId } },
+        { new: true, runValidators: true }
+      );
+
+      await User.findOneAndUpdate(
+        { _id: friendId },
+        { $pull: { friends: userId } },
+        { new: true, runValidators: true }
+      );
+      return res.json({ success: true });
+    } else res.status(500).json({ success: false });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to delete friend | ${error.message}`);
+    return res.status(500).json({ success: false, error: error.message });
+  }
 };
 
 module.exports = { createNewFriend, deleteFriend };
